51a42bf87927c6f8878db36688240d29b011c272
DanielConduri <jonathand.tene@espoch.edu.ec>
2024-06-10 19:26:45 -0500

    Actualización de cors

--- server/src/ServiciosInventarios.js
+++ server/src/ServiciosInventarios.js
@@ -6,65 +6,65 @@ dotenv.config()
 import https from "https";
 import fs from "fs";
 
-async function main(port){
-  try {
-  await sequelize.sync( { /*force: true*/ }); //No recrea las tablas
-      console.log('Connection has been established successfully.');
-      app.listen(port, () => {
-          console.log("Server is listening on port", port);
-        });
-      //app.listen(3002)
-      //console.log('Server is  listening on port', 3002)
-  } catch (error) {
-      console.error('Unable to connect to the database:', error);
-  }
-}
-
-main(configVariables.port);
-
-
-// async function main(port) {
+// async function main(port){
 //   try {
-
-//     await sequelize.sync({  logging: false });
-
-//     sequelize
-//       .authenticate()
-//       .then(() => {
-//         console.log("Conexion Exitosa a la base.");
-//       })
-//       .catch((error) => {
-//         console.error("Error en la conexion a la base:", error);
-//       });
-
-     
-//     console.log("usuario y clave", configVariables.port);
-//     console.log("NODE_ENV: ", process.env.NODE_ENV);
-//     if (process.env.NODE_ENV !== "production") {
-//       try {
-//         const options = {
-//           key: fs.readFileSync(
-//             "./src/assets/Certificados/STAR_espoch_edu_ec.key"
-//           ),
-//           cert: fs.readFileSync(
-//             "./src/assets/Certificados/STAR_espoch_edu_ec.crt"
-//           ),
-//         };
-//         https.createServer(options, app).listen(port, "0.0.0.0");
-//         console.log("si ingresa")
-//       } catch (error) {
-//         console.error("Is not a DEV enviroment: ", error);
-//       }
-//     } else {
-//       console.log("\n\n>> PRODUCTION\n\n");
+//   await sequelize.sync( { /*force: true*/ }); //No recrea las tablas
+//       console.log('Connection has been established successfully.');
 //       app.listen(port, () => {
-//         console.log("Servicios Inventarios levantado exitosamente: ", port)
-
-//       });
-//     }
+//           console.log("Server is listening on port", port);
+//         });
+//       //app.listen(3002)
+//       //console.log('Server is  listening on port', 3002)
 //   } catch (error) {
-//     console.error("Unable to connect to the database:", error);
+//       console.error('Unable to connect to the database:', error);
 //   }
 // }
 
 // main(configVariables.port);
+
+
+async function main(port) {
+  try {
+
+    await sequelize.sync({  logging: false });
+
+    sequelize
+      .authenticate()
+      .then(() => {
+        console.log("Conexion Exitosa a la base.");
+      })
+      .catch((error) => {
+        console.error("Error en la conexion a la base:", error);
+      });
+
+     
+    console.log("usuario y clave", configVariables.port);
+    console.log("NODE_ENV: ", process.env.NODE_ENV);
+    if (process.env.NODE_ENV !== "production") {
+      try {
+        const options = {
+          key: fs.readFileSync(
+            "./src/assets/Certificados/STAR_espoch_edu_ec.key"
+          ),
+          cert: fs.readFileSync(
+            "./src/assets/Certificados/STAR_espoch_edu_ec.crt"
+          ),
+        };
+        https.createServer(options, app).listen(port, "0.0.0.0");
+        console.log("si ingresa")
+      } catch (error) {
+        console.error("Is not a DEV enviroment: ", error);
+      }
+    } else {
+      console.log("\n\n>> PRODUCTION\n\n");
+      app.listen(port, () => {
+        console.log("Servicios Inventarios levantado exitosamente: ", port)
+
+      });
+    }
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+}
+
+main(configVariables.port);
--- server/src/app.js
+++ server/src/app.js
@@ -7,7 +7,7 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://inventarios.espoch.edu.ec/');
+  res.header('Access-Control-Allow-Origin', 'https://inventario-espoch.vercel.app');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
