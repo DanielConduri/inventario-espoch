fd51f28b697612157cfc4b9b6570c70842994b4a
DanielConduri <jonathand.tene@espoch.edu.ec>
2024-09-28 19:00:21 -0500

    database server update v2.2

--- server/src/ServiciosInventarios.js
+++ server/src/ServiciosInventarios.js
@@ -23,47 +23,60 @@ import fs from "fs";
 // main(configVariables.port);
 
 
-async function main(port) {
-  try {
+// async function main(port) {
+//   try {
 
-    await sequelize.sync({  logging: false });
+//     await sequelize.sync({  logging: false });
 
-    sequelize
-      .authenticate()
-      .then(() => {
-        console.log("Conexion Exitosa a la base.");
-      })
-      .catch((error) => {
-        console.error("Error en la conexion a la base:", error);
-      });
+//     sequelize
+//       .authenticate()
+//       .then(() => {
+//         console.log("Conexion Exitosa a la base.");
+//       })
+//       .catch((error) => {
+//         console.error("Error en la conexion a la base:", error);
+//       });
 
      
-    console.log("usuario y clave", configVariables.port);
-    console.log("NODE_ENV: ", process.env.NODE_ENV);
-    if (process.env.NODE_ENV !== "production") {
-      try {
-        const options = {
-          key: fs.readFileSync(
-            "./src/assets/Certificados/STAR_espoch_edu_ec.key"
-          ),
-          cert: fs.readFileSync(
-            "./src/assets/Certificados/STAR_espoch_edu_ec.crt"
-          ),
-        };
-        https.createServer(options, app).listen(port, "0.0.0.0");
-        console.log("si ingresa")
-      } catch (error) {
-        console.error("Is not a DEV enviroment: ", error);
-      }
-    } else {
-      console.log("\n\n>> PRODUCTION\n\n");
-      app.listen(port, () => {
-        console.log("Servicios Inventarios levantado exitosamente: ", port)
+//     console.log("usuario y clave", configVariables.port);
+//     console.log("NODE_ENV: ", process.env.NODE_ENV);
+//     if (process.env.NODE_ENV !== "production") {
+//       try {
+//         const options = {
+//           key: fs.readFileSync(
+//             "./src/assets/Certificados/STAR_espoch_edu_ec.key"
+//           ),
+//           cert: fs.readFileSync(
+//             "./src/assets/Certificados/STAR_espoch_edu_ec.crt"
+//           ),
+//         };
+//         https.createServer(options, app).listen(port, "0.0.0.0");
+//         console.log("si ingresa")
+//       } catch (error) {
+//         console.error("Is not a DEV enviroment: ", error);
+//       }
+//     } else {
+//       console.log("\n\n>> PRODUCTION\n\n");
+//       app.listen(port, () => {
+//         console.log("Servicios Inventarios levantado exitosamente: ", port)
 
-      });
-    }
+//       });
+//     }
+//   } catch (error) {
+//     console.error("Unable to connect to the database:", error);
+//   }
+// }
+async function main(port){
+  try {
+  await sequelize.sync( {}); //No recrea las tablas
+      console.log('Connection has been established successfully.');
+      app.listen(port, () => {
+          console.log("Server is listening on port", port);
+        });
+      //app.listen(3002)
+      //console.log('Server is  listening on port', 3002)
   } catch (error) {
-    console.error("Unable to connect to the database:", error);
+      console.error('Unable to connect to the database:', error);
   }
 }
 
